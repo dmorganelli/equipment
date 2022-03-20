391a4ca2cd0473b2cbde24dd85cd01aefb560c4d
lyfe606 <lyfe606@yahoo.com>
2022-03-20 13:14:24 -0400

    Added setter and getter for defaultChannel.

--- PowerSupply/rigol_channelVisa.py
+++ PowerSupply/rigol_channelVisa.py
@@ -9,20 +9,33 @@ Created on Sun Mar 20 12:23:38 2022
 class apply():
     def __init__(self,visaCommands):
         self.visaCommands = visaCommands
-        self.defaultChannel = 'CH1'
+        self._defaultChannel = 'CH1'
         self.startString = ':APPL';
 
     def ch1(self):
-        self.defaultChannel = 'CH1'
+        self.defaultChannel = 1;
         self.visaCommands._writeVal(self.startString)
 
     def ch2(self):
-        self.defaultChannel = 'CH2'
+        self.defaultChannel = 2;
         self.visaCommands._writeVal(self.startString)
 
     def ch3(self):
-        self.defaultChannel = 'CH3'
+        self.defaultChannel = 3;
         self.visaCommands._writeVal(self.startString)
+
+    @property #getter 
+    def defaultChannel(self):
+        return self._defaultChannel
+
+    @defaultChannel.setter
+    def defaultChannel(self,channel = 1):
+        if channel: #channel in empty when imported. this could be avoided with __main__.
+            if (channel >= 1 ) & (channel <= 3):
+                self._defaultChannel = 'CH'+str(channel)
+                self.visaCommands._writeVal(self.startString)
+            else:
+                print('Not a valid Channel. Defaults is still set to '+self._defaultChannel)
 
     def settingsDefaultCh(self):
         if not self.defaultChannel:
